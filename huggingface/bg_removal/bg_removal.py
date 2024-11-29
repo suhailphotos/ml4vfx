6b527dad38d300213abe1d482ee943a3385bc20f
pixelknit <felipecgdesign@gmail.com>
2024-11-29 09:16:24 +0000

    huggingface files

---
+++ huggingface/bg_removal/bg_removal.py
@@ -0,0 +1,34 @@
+from PIL import Image
+import matplotlib.pyplot as plt
+import torch
+from torchvision import transforms
+from transformers import AutoModelForImageSegmentation
+
+input_image_path = "./person1.png"
+
+model = AutoModelForImageSegmentation.from_pretrained('briaai/RMBG-2.0', trust_remote_code=True)
+torch.set_float32_matmul_precision(['high', 'highest'][0])
+model.to('cuda')
+model.eval()
+
+# Data settings
+image_size = (1024, 1024)
+transform_image = transforms.Compose([
+    transforms.Resize(image_size),
+    transforms.ToTensor(),
+    transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
+])
+
+image = Image.open(input_image_path)
+input_images = transform_image(image).unsqueeze(0).to('cuda')
+
+# Prediction
+with torch.no_grad():
+    preds = model(input_images)[-1].sigmoid().cpu()
+pred = preds[0].squeeze()
+pred_pil = transforms.ToPILImage()(pred)
+mask = pred_pil.resize(image.size)
+image.putalpha(mask)
+
+image.save("no_bg_image.png")
+
